79ec33f898878dc76b849c91631fd7ed4da944b5
Vashani verma <144155899+Vashaniverma@users.noreply.github.com>
2026-07-20 17:29:18 +0530

    Time: 9 ms (16.09%) | Memory: 47.4 MB (26.21%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -1,74 +1,25 @@
 class Solution {
-    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+  public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+    final int m = grid.length;
+    final int n = grid[0].length;
+    List<List<Integer>> ans = new ArrayList<>();
+    int[][] arr = new int[m][n];
 
-        int row = grid.length;
-        int col = grid[0].length;
+    k %= m * n;
 
-        int[] nums = new int[row * col];
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j) {
+        final int index = (i * n + j + k) % (m * n);
+        final int x = index / n;
+        final int y = index % n;
+        arr[x][y] = grid[i][j];
+      }
 
-        k = k % nums.length;
+    for (int[] row : arr)
+      ans.add(Arrays.stream(row).boxed().collect(Collectors.toList()));
 
-        int[] temp = new int[k];
-
-        int i = 0;
-        int j = 0;
-        int count = 0;
-
-        // Convert 2D grid to 1D array
-        while(i < row){
-
-            nums[count] = grid[i][j];
-
-            count++;
-            j++;
-
-            if(j >= col){
-                j = 0;
-                i++;
-            }
-        }
-
-        // Store last k elements
-        int find = nums.length - k;
-
-        int idx = 0;
-
-        for(int l = find; l < nums.length; l++){
-
-            temp[idx] = nums[l];
-            idx++;
-        }
-
-        // Shift remaining elements
-        for(int l = find - 1; l >= 0; l--){
-
-            nums[l + k] = nums[l];
-        }
-
-        // Put last k elements at front
-        for(int l = 0; l < temp.length; l++){
-
-            nums[l] = temp[l];
-        }
-
-        // Convert back to List<List<Integer>>
-        List<List<Integer>> result = new ArrayList<>();
-
-        count = 0;
-
-        for(i = 0; i < row; i++){
-
-            List<Integer> tempList = new ArrayList<>();
-
-            for(j = 0; j < col; j++){
-
-                tempList.add(nums[count]);
-                count++;
-            }
-
-            result.add(tempList);
-        }
-
-        return result;
-    }
+    return ans;
+  }
 }
+
+
